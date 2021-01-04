34d74f050dd211c4c0f0049e10dd0acd2d3fca6f
ricardovg4 <ricardo.velasco.guachalla@gmail.com>
2021-01-03 22:27:07 -0400

    custom domain

--- client/src/apiCalls/baseUrl.js
+++ client/src/apiCalls/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
     process.env.NODE_ENV === 'production'
-        ? 'http://104.236.12.233'
+        ? // ? 'http://104.236.12.233'
+          'https://api.lereportmanager.com'
         : 'http://localhost:5000';
 
 export default baseUrl;
