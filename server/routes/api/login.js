db4e0fd68fc4b51ac68e4974021d2bcb8632e77f
ricardovg4 <ricardo.velasco.guachalla@gmail.com>
2021-01-07 17:28:07 -0400

    cookie secure

--- server/routes/api/login.js
+++ server/routes/api/login.js
@@ -37,7 +37,7 @@ router.post('/', async (req, res) => {
                 res.cookie(jwtCookieName, token, {
                     httpOnly: true,
                     sameSite: 'strict',
-                    // secure: true,
+                    secure: secure,
                     maxAge: maxAge * 1000
                 });
                 return res.status(200).json({
